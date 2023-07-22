f75d152401a7e9824082f26722397afb2fb6971a
Aayush Singh <aayush_singh@mymail.sutd.edu.sg>
2023-07-22 22:51:33 +0800

    Update templates.R

--- templates.R
+++ templates.R
@@ -10,15 +10,15 @@
 passwordModal <- function(failed = FALSE) {
   modalDialog(
     title = "Create a New Player!",
-    textInput("playername", "Enter your desired Player Name:"),
-    passwordInput("password1", "Enter a new password:"),
-    passwordInput("password2", "Confirm by re-entering the new password:"),
+    textInput("registerUsername", "Enter your desired Player Name:"),
+    passwordInput("registerPassword", "Enter a new password:"),
+    passwordInput("registerPasswordConfirmation", "Confirm by re-entering the new password:"),
     if (failed)
       div(tags$b("The passwords do not match. Try again.", style = "color: red;")),
     
     footer = tagList(
       modalButton("Cancel"),
-      actionButton("passwordok", "OK")
+      actionButton("registerButton", "OK")
     )
   )
 }
@@ -26,14 +26,14 @@ passwordModal <- function(failed = FALSE) {
 loginModal <- function(failed = FALSE) {
   modalDialog(
     title = "Login",
-    textInput("playername", "Enter your assigned Player Name:"),
-    passwordInput("password3", "Enter your password:"),
+    textInput("loginUsername", "Enter your assigned Player Name:"),
+    passwordInput("loginPassword", "Enter your password:"),
     if (failed)
       div(tags$b("There is no registered player with that name and password. Try again or re-register.", style = "color: red;")),
     
     footer = tagList(
       modalButton("Cancel"),
-      actionButton("loginok", "OK")
+      actionButton("loginButton", "OK")
     )
   )
 }
