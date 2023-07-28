e6ef1bc7e1dde0da468841596bf422eb8973261f
ffannisa <fannisafahmi3@gmail.com>
2023-07-28 15:54:18 +0800

    edit

--- src/app/www/scripts/analytics-events.js
+++
@@ -1,5 +0,0 @@
-let sendAnalyticsEvent = function(options) {
-  gtag('event', options.action, {'event_category' : options.category,
-    'event_label' : options.label});
-}
-Shiny.addCustomMessageHandler('trackEvent', sendAnalyticsEvent)
