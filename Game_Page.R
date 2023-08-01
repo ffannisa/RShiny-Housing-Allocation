de9520eec0e3013292f7ba527ddb751ddf9f7502
ffannisa <fannisafahmi3@gmail.com>
2023-08-01 16:57:47 +0800

    new edits

--- Game_Page.R
+++ Game_Page.R
@@ -4,36 +4,6 @@ library(tidyverse)
 library(shinyWidgets)
 library(shinyjs)
 
-# Happiness Index using Gauge Chart
-gg.gauge <- function(pos,breaks=c(0,30,70,100)) {
-  require(ggplot2)
-  get.poly <- function(a,b,r1=0.5,r2=1.0) {
-    th.start <- pi*(1-a/100)
-    th.end   <- pi*(1-b/100)
-    th       <- seq(th.start,th.end,length=100)
-    x        <- c(r1*cos(th),rev(r2*cos(th)))
-    y        <- c(r1*sin(th),rev(r2*sin(th)))
-    return(data.frame(x,y))
-  }
-  ggplot()+ 
-    geom_polygon(data=get.poly(breaks[1],breaks[2]),aes(x,y),fill="red")+
-    geom_polygon(data=get.poly(breaks[2],breaks[3]),aes(x,y),fill="gold")+
-    geom_polygon(data=get.poly(breaks[3],breaks[4]),aes(x,y),fill="forestgreen")+
-    geom_polygon(data=get.poly(pos-1,pos+1,0.2),aes(x,y))+
-    geom_text(data=as.data.frame(breaks), size=5, fontface="bold", vjust=0,
-              aes(x=1.1*cos(pi*(1-breaks/100)),y=1.1*sin(pi*(1-breaks/100)),label=paste0(breaks,"%")))+
-    annotate("text",x=0,y=0,label=pos,vjust=0,size=8,fontface="bold")+
-    coord_fixed()+
-    theme_bw()+
-    theme(axis.text=element_blank(),
-          axis.title=element_blank(),
-          axis.ticks=element_blank(),
-          panel.grid=element_blank(),
-          panel.border=element_blank()) 
-}
-gg.gauge(52,breaks=c(0,35,70,100))
-
-
 ui <- fluidPage(
   # Add font and Game design template to use -> Nes.css
   # Link the external CSS file and fonts
@@ -115,6 +85,9 @@ ui <- fluidPage(
                                      )
                         )
                  ),
+                 br(),
+                 br(),
+                 br(),
                  column(width = 5,
                         # Value Box 5 (Happiness)
                         div(outputId = "box_5", class = "custom-value-box5",
@@ -158,7 +131,7 @@ ui <- fluidPage(
                               column(2, strong('Additional Content 3')),
                               column(2, strong('Additional Content 4')),
                               column(2, strong('Additional Content 5')),
-                              column(2, strong('Additional Content 6')),
+                              column(2, actionButton("build", label = "Build!", class = "nes-btn is-success")),
                             )
                           )
                         )
